72df8281c621187602b578187090b42a55dda35a
Kelvin Harron <kelvinharron@outlook.com>
2016-11-19 13:30:48 +0000

    Added a welcome screen text and edit text box, java code gets the goal string and makes a toast commented out intent ready for add new goal activity

--- app/src/main/java/com/example/kelvinharron/mind_body_bro/MainActivity.java
+++ app/src/main/java/com/example/kelvinharron/mind_body_bro/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.kelvinharron.mind_body_bro;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final EditText goalEditText = (EditText) findViewById(R.id.goalEditText);
+        goalEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (i == KeyEvent.KEYCODE_ENTER)) {
+                    
+                    Toast.makeText(MainActivity.this, goalEditText.getText(), Toast.LENGTH_SHORT).show();
+                    // HERE WE GET THE TEXT VALUE, NEED TO USE INTENT TO PASS TO ADD NEW GOAL ACTIVITY
+                    // Intent addNewGoalIntent = new Intent(MainActivity.this, AddNewGoalActivity.class);
+                    //addNewGoalIntent.putExtra("TextBox", goalEditText.getText().toString());
+                    //startActivity(addNewGoalIntent);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
